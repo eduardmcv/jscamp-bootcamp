a8e1ead605a19b1aaa34e08a137f57a1f96f61d7
eduardmcv <eduardomacovei@gmail.com>
2025-10-28 05:54:11 +0100

    Se escucha el click dentro del contener padre y si el click es sobre un botón con una clase especifica, se le aplican estilos

---
+++ 02-javascript/script.js
@@ -0,0 +1,33 @@
+// const botonAplicar = document.querySelector('.cta-button');
+
+// botonAplicar.addEventListener('click', function() {
+//     botonAplicar.textContent = '¡Aplicado!';
+//     botonAplicar.classList.add('is-applied');
+// })
+
+
+
+// const botonesAplicar = document.querySelectorAll('.cta-button');
+
+// botonesAplicar.forEach(boton => {
+//     boton.addEventListener('click', function () {
+//         boton.textContent = '¡Aplicado!';
+//         boton.classList.add('is-applied');
+//         boton.disabled = true;
+//     });
+// });
+
+
+
+const displayOfertas = document.querySelector('.display-ofertas');
+
+displayOfertas.addEventListener('click', function (event) {
+    const element = event.target;
+
+    if (element.classList.contains('cta-button')) {
+        element.textContent = '¡Aplicado!';
+        element.classList.add('is-applied');
+        element.disabled = true;
+    }
+
+});
