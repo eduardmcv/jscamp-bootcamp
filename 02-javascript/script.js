61887a461d793108a9322fa0ab0d82956fcf2197
eduardmcv <eduardomacovei@gmail.com>
2025-10-29 06:40:21 +0100

    Fetch to Json

--- 02-javascript/script.js
+++ 02-javascript/script.js
@@ -21,7 +21,7 @@
 
 const displayOfertas = document.querySelector('.display-ofertas');
 
-displayOfertas.addEventListener('click', function (event) {
+displayOfertas?.addEventListener('click', function (event) {
     const element = event.target;
 
     if (element.classList.contains('cta-button')) {
@@ -31,3 +31,54 @@ displayOfertas.addEventListener('click', function (event) {
     }
 
 });
+
+
+const filterTechnology = document.querySelector('#filter-technology');
+
+filterTechnology?.addEventListener('change', function () {
+    const selectedValue = filterTechnology.value;
+    console.log('Filtro de tecnología seleccionado:', selectedValue);
+});
+
+const filterUbication = document.querySelector('#filter-ubication');
+
+filterUbication?.addEventListener('change', function () {
+    const selectedValue = filterUbication.value;
+    console.log('Filtro de ubicación seleccionado:', selectedValue);
+});
+
+const filterExperience = document.querySelector('#filter-experience');
+
+filterExperience?.addEventListener('change', function () {
+    const selectedValue = filterExperience.value;
+    console.log('Filtro de experiencia seleccionado:', selectedValue);
+});
+
+
+
+fetch("./data.json")
+    .then((response) => {
+        return response.json()
+    })
+    .then((jobs) => {
+        jobs.forEach(job => {
+            const article = document.createElement('article');
+            article.className = 'oferta';
+
+            article.dataset.technology = job.data.technology;
+            article.dataset.ubication = job.data.modalidad;
+            article.dataset.experience = job.data.nivel;
+
+            article.innerHTML = `
+                <div>
+                    <header class="header-oferta">
+                        <h3>${job.titulo}</h3>
+                        <p>${job.empresa} | ${job.ubicacion}</p>
+                    </header>
+                    <p>${job.descripcion}</p>
+                </div>
+            `;
+            
+            displayOfertas.appendChild(article);
+        });
+    })
