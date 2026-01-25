b680ab01af50c62d5a49087dd83b91d6b75d36da
eduardmcv <eduardomacovei@gmail.com>
2026-01-25 14:15:17 +0100

    Eliminar carpetas innecesarias y reorganizar ejercicios

--- 02-ejercicio-javascript/dudas.md
+++
@@ -1,1 +0,0 @@
-<!-- Aquí puedes introducir tus dudas sobre el ejercicio, la consigna, la corrección, etc -->
--- 02-ejercicio-javascript/fetch-data.js
+++ 02-ejercicio-javascript/fetch-data.js
@@ -1,1 +1,31 @@
-/* Aquí va la lógica para mostrar los resultados de búsqueda */
+const displayOfertas = document.querySelector('.display-ofertas');
+
+fetch("./data.json")
+    .then((response) => {
+        return response.json()
+    })
+    .then((jobs) => {
+        jobs.forEach(job => {
+            const article = document.createElement('article');
+            article.className = 'oferta';
+
+            article.dataset.technology = job.data.technology;
+            article.dataset.location = job.data.modalidad;
+            article.dataset.experience = job.data.nivel;
+
+            article.innerHTML = `
+                <div>
+                    <header class="header-oferta">
+                        <h3 class="job-title">${job.titulo}</h3>
+                        <p>${job.empresa} | ${job.ubicacion}</p>
+                    </header>
+                    <p>${job.descripcion}</p>
+                </div>
+                <div>
+                    <button class="cta-button" href="">Aplicar</button>
+                </div>
+            `;
+
+            displayOfertas.appendChild(article);
+        });
+    })
